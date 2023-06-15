7cb797577f4f8c93febe0a9aeaf88af5cc93760f
tuanacetinkaya <tuanacetinkaya7@gmail.com>
2023-06-15 16:39:12 +0300

    allow full screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,21 +26,21 @@ const Home = () => {
         <h1>Demo Videos</h1>
         <h2>Mapping</h2>
         <div className="embed-responsive embed-responsive-16by9">
-          <iframe src='' title="Mapping Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe src='' title="Mapping Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
         <h2>Navigation</h2>
         <div className="embed-responsive embed-responsive-16by9">
-          <iframe src='' title="Navigation Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe src='' title="Navigation Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
         <h2>Indoor Object Avoidance</h2>
         <p>The local cost map and the local plan extend 3 meters from the robot but the global map extends 100 meters. This is because we want to be able to update the local cost map much more frequently to have dynamic obstacle avoidance capability and want to have a 100 meters global range. Since the robot cannot navigate to a place where it is outside the global costmap we set up the global map’s range of 100 meters.</p>
         <div className="embed-responsive embed-responsive-16by9">
-          <iframe src='' title="Mapping Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe src='' title="Mapping Demo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
         <h2>Outdoor Object Avoidance</h2>
         <p>Robot is also capable of outdoor mapping while avoiding obstacles.</p>
         <div className="embed-responsive embed-responsive-16by9">
-          <iframe src='' title="Object Avoidance" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe src='' title="Object Avoidance" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         </div>
       </div>
       <About data={landingPageData.About} link="https://www.youtube.com/embed/N9Acz7egQyA" />
